34b42324bf99a88de60f97f81fcec49f75f53319
Jelani Brandon <jelanibrandon@gmail.com>
2021-11-27 21:36:57 -0800

    Initial addition of submodules and cmakes

---
+++ client/src/client_main.c
@@ -0,0 +1,12 @@
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main()
+{
+    int result = 0;
+    return result;
+}
